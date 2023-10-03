45d852f1280ebe33f94521fa1141a632039a8aa7
John Steban Lopez <104475729+JohnGolgota@users.noreply.github.com>
2023-10-03 12:11:18 -0500

    no entiendo una monda que hacia kolozuz

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 pub mod pet;
+pub mod menu;
 use pet::Pet;
-use std::{thread, time};
+use std::{thread, time, sync::mpsc};
 use rand::Rng;
 
+use crate::menu::Menu;
 use crate::pet::PetStatus;
 
 fn main() {
 	let mut rng = rand::thread_rng();
     let mut mascota = Pet::new(String::from("perro"));
+	let menu: Menu = Menu::new();
     let frames = [
         r#"
         /\_/\
@@ -54,17 +57,31 @@ fn main() {
 	"#;
     const MILLIS: u64 = 600;
     let mut msg:&str = "meaw";
+	let (tx, rx) = mpsc::channel();
+
+	let input_handle = thread::spawn(move || {
+		let mut input = String::new();
+		let result = std::io::stdin().read_line(&mut input);
+		if result.is_ok() {
+			tx.send(input).unwrap();
+		}
+	});
 
     for frame in frames.iter().cycle() {
-		clear_terminal();
+		// clear_terminal();
 		let number:u8 = rng.gen_range(0..=10);
 		println!("Salud: {}, Hambre: {}, Higiene: {}", mascota.stats.health, mascota.stats.hunger, mascota.stats.dirtiness);
         if mascota.status == PetStatus::Death {
-			print!("{}\nTu {} ha muerto :(", kato_muerto, mascota.name);
+			print!("{}\nRIP {} :(", kato_muerto, mascota.name);
             break;
         }
+		print!("{}", menu.options);
 		print!("{}", frame);
 		println!("\n{} ..", msg);
+
+		let select = rx.try_recv();
+		print!("\nselect: {:?}\n", select);
+
         if number % 2u8 == 0 {
             msg = "..";
         } else {
---
+++ src/menu.rs
@@ -0,0 +1,11 @@
+pub struct Menu {
+    // items: Vec<String>,
+    pub options: String,
+}
+impl Menu {
+    pub fn new() -> Menu {
+        Menu {
+            options: String::from("1. Feed\n2. Play\n3. Sleep\n4. Wash\n5. Exit"),
+        }
+    }
+}
