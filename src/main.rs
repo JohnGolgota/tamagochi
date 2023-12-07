d46289ce0882315fedb2c8eb441032c2b27bc720
Kolozuz <moralesjuanpablo1122@gmail.com>
2023-12-07 15:52:17 -0500

    Added grid example

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 pub mod menu;
 pub mod pet;
+mod utils;
+use utils::Grid;
 use pet::Pet;
 use rand::Rng;
+use std::{array, vec};
 use std::io::{self, Write};
 use std::{sync::mpsc, thread, time};
 
@@ -56,53 +59,63 @@ fn main() {
  /   \    _
 /|_|_|\__/
 	"#;
-    const MILLIS: u64 = 600;
-    let mut msg: &str = "meaw";
-    let (tx, rx) = mpsc::channel();
-    let input_handle = thread::spawn(move || {
-        loop {
-            let mut input = String::new();
-            io::stdout().flush().unwrap();
-            io::stdin().read_line(&mut input).unwrap();
-            tx.send(input.trim().to_string()).unwrap();
-            if input.trim() == "exit" {
-                break;
-            }
+    
+    let grid:Vec<Vec<&str>> = Grid::new(12, 6);
+    for row in grid {
+        let mut rowstr: String = String::new();
+        for cell  in row {
+            rowstr += cell;
         }
-    });
-    for frame in frames.iter().cycle() {
-        clear_terminal();
-        let number: u8 = rng.gen_range(0..=10);
-        println!(
-            "Salud: {}, Hambre: {}, Higiene: {}, Aburrimiento: {}, Cansancio: {}, Suciedad: {}",
-            mascota.stats.health, mascota.stats.hunger, mascota.stats.dirtiness, mascota.stats.boredom, mascota.stats.tiredness, mascota.stats.dirtiness
-        );
-        if mascota.status == PetStatus::Death {
-            print!("{}\nRIP {} :(", kato_muerto, mascota.name);
-            break;
-        }
-        print!("{}", menu.options);
-        print!("{}", frame);
-        let select = rx.recv().unwrap();
-        match select.as_str() {
-            "1" => mascota.feed(),
-            "2" => mascota.play(),
-            "3" => mascota.sleep(),
-            "4" => mascota.wash(),
-            "5" => break,
-            _ =>  println!("{}", msg),
-        }
-        if number % 2u8 == 0 {
-            msg = "..";
-        } else {
-            msg = "meaw";
-        }
-        thread::sleep(time::Duration::from_millis(MILLIS));
-        mascota.live();
+        println!("{}", rowstr);
     }
-	input_handle.join().unwrap();
-}
 
-fn clear_terminal() {
-    print!("\x1B[2J\x1B[1;1H");
+//     const MILLIS: u64 = 600;
+//     let mut msg: &str = "meaw";
+//     let (tx, rx) = mpsc::channel();
+//     let input_handle = thread::spawn(move || {
+//         loop {
+//             let mut input = String::new();
+//             io::stdout().flush().unwrap();
+//             io::stdin().read_line(&mut input).unwrap();
+//             tx.send(input.trim().to_string()).unwrap();
+//             if input.trim() == "exit" {
+//                 break;
+//             }
+//         }
+//     });
+//     for frame in frames.iter().cycle() {
+//         clear_terminal();
+//         let number: u8 = rng.gen_range(0..=10);
+//         println!(
+//             "Salud: {}, Hambre: {}, Higiene: {}, Aburrimiento: {}, Cansancio: {}, Suciedad: {}",
+//             mascota.stats.health, mascota.stats.hunger, mascota.stats.dirtiness, mascota.stats.boredom, mascota.stats.tiredness, mascota.stats.dirtiness
+//         );
+//         if mascota.status == PetStatus::Death {
+//             print!("{}\nRIP {} :(", kato_muerto, mascota.name);
+//             break;
+//         }
+//         print!("{}", menu.options);
+//         print!("{}", frame);
+//         let select = rx.recv().unwrap();
+//         match select.as_str() {
+//             "1" => mascota.feed(),
+//             "2" => mascota.play(),
+//             "3" => mascota.sleep(),
+//             "4" => mascota.wash(),
+//             "5" => break,
+//             _ =>  println!("{}", msg),
+//         }
+//         if number % 2u8 == 0 {
+//             msg = "..";
+//         } else {
+//             msg = "meaw";
+//         }
+//         thread::sleep(time::Duration::from_millis(MILLIS));
+//         mascota.live();
+//     }
+// 	input_handle.join().unwrap();
+// }
+
+// fn clear_terminal() {
+//     print!("\x1B[2J\x1B[1;1H");
 }
